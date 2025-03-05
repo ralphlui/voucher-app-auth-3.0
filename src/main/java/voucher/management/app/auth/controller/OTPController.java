95da7812ac253ccdbead843d52aeb19e32ae7d84
Thet-Nandar-Aung <e1327955@u.nus.edu>
2025-03-06 01:07:05 +0800

    Add audit log for OTP Generate and Validate API.

--- src/main/java/voucher/management/app/auth/controller/OTPController.java
+++ src/main/java/voucher/management/app/auth/controller/OTPController.java
@@ -3,7 +3,9 @@ package voucher.management.app.auth.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,10 @@ import voucher.management.app.auth.dto.UserDTO;
 import voucher.management.app.auth.dto.UserRequest;
 import voucher.management.app.auth.dto.ValidationResult;
 import voucher.management.app.auth.entity.User;
+import voucher.management.app.auth.enums.AuditLogInvalidUser;
+import voucher.management.app.auth.enums.AuditLogResponseStatus;
+import voucher.management.app.auth.exception.UserNotFoundException;
+import voucher.management.app.auth.service.impl.AuditLogService;
 import voucher.management.app.auth.service.impl.OTPStorageService;
 import voucher.management.app.auth.service.impl.UserService;
 import voucher.management.app.auth.strategy.impl.UserValidationStrategy;
@@ -28,49 +34,82 @@ public class OTPController {
 	@Autowired
 	private UserValidationStrategy userValidationStrategy;
 
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private AuditLogService auditLogService;
+
+	private String auditLogResponseSuccess = AuditLogResponseStatus.SUCCESS.toString();
+	private String auditLogResponseFailure = AuditLogResponseStatus.FAILED.toString();
+	private String auditLogUserId = AuditLogInvalidUser.InvalidUserID.toString();
+	private String auditLogUserName = AuditLogInvalidUser.InvalidUserName.toString();
+	private String genericErrorMessage = "An error occurred while processing your request. Please try again later.";
+
 	@PostMapping("/generate")
-	public ResponseEntity<APIResponse<UserDTO>> generateOtp(@RequestBody UserRequest userRequest) {
+	public ResponseEntity<APIResponse<UserDTO>> generateOtp(@RequestHeader("X-User-Id") String userID,
+			@RequestBody UserRequest userRequest) {
+
+		String message = "";
+		String activityType = "Authentication-GenerateOTP";
+		String apiEndPoint = "api/opt";
+		String httpMethod = HttpMethod.POST.name();
+		String activityDesc = "Generating OTP is failed due to ";
+
 		try {
 			// check userEmail is valid
 			logger.info("Reset Password : " + userRequest.getEmail());
 			ValidationResult validationResult = userValidationStrategy.validateObject(userRequest.getEmail());
 			if (!validationResult.isValid()) {
 
-				logger.error("generateOtp Error: " + validationResult.getMessage());
-				return ResponseEntity.status(validationResult.getStatus())
-						.body(APIResponse.error(validationResult.getMessage()));
+				logger.error("Generate OTP validation is not successful");
+				return handleResponseAndsendAuditLogForValidationFailure(validationResult, activityType, activityDesc,
+						apiEndPoint, httpMethod);
 			}
 
 			int otp = otpService.generateAndStoreOTP(userRequest.getEmail());
 			// otpService.sendOTPEmail(email, otp);
 
-			String message = "";
 			if (otp > 0) {
-				message = "OTP sent to "+otp + userRequest.getEmail() + ". It is valid for 10 minutes.";
+				message = "OTP sent to "+ userRequest.getEmail() + ". It is valid for 10 minutes.";
 			}
-			return ResponseEntity.status(HttpStatus.OK).body(APIResponse.success(message));
+			// otpService.sendOTPEmail(email, otp);
+			UserDTO userDTO = userService.checkSpecificActiveUser(userID);
+
+			return handleResponseAndsendAuditLogForSuccessCase(userDTO, activityType, message, apiEndPoint, httpMethod);
 
 		} catch (Exception e) {
-			String message = "OTP code generation failed.";
+			message = "OTP code generation failed.";
 			logger.error("generateOtp Error: " + message);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(APIResponse.error(message));
+			HttpStatusCode htpStatuscode = e instanceof UserNotFoundException ? HttpStatus.NOT_FOUND
+					: HttpStatus.INTERNAL_SERVER_ERROR;
+			return handleResponseAndsendAuditLogForExceptionCase(e, htpStatuscode, activityType, activityDesc,
+					apiEndPoint, httpMethod);
 		}
 	}
 
 	@PostMapping("/validate")
-	public ResponseEntity<APIResponse<UserDTO>> validateOtp(@RequestBody UserRequest userRequest) {
+	public ResponseEntity<APIResponse<UserDTO>> validateOtp(@RequestHeader("X-User-Id") String userID,
+			@RequestBody UserRequest userRequest) {
+
+		String message = "";
+		String activityType = "Authentication-GenerateOTP";
+		String apiEndPoint = "api/opt";
+		String httpMethod = HttpMethod.POST.name();
+		String activityDesc = "Validating OTP is failed due to ";
+
 		try {
 
 			logger.info("Reset Password : " + userRequest.getEmail());
 			ValidationResult validationResult = userValidationStrategy.validateObject(userRequest.getEmail());
 			if (!validationResult.isValid()) {
 
-				logger.error("generateOtp Error: " + validationResult.getMessage());
-				return ResponseEntity.status(validationResult.getStatus())
-						.body(APIResponse.error(validationResult.getMessage()));
+				logger.error("Generate OTP validation is not successful");
+				return handleResponseAndsendAuditLogForValidationFailure(validationResult, activityType, activityDesc,
+						apiEndPoint, httpMethod);
 			}
 
-			String message = "";
+			message = "";
 			boolean isValid = otpService.validateOTP(userRequest.getEmail(), userRequest.getOtp());
 			if (isValid) {
 				message = "OTP is valid.";
@@ -78,13 +117,55 @@ public class OTPController {
 				message = "OTP expired or incorrect";
 			}
 
-			return ResponseEntity.status(HttpStatus.OK).body(APIResponse.success(message));
+			UserDTO userDTO = userService.checkSpecificActiveUser(userID);
+
+			return handleResponseAndsendAuditLogForSuccessCase(userDTO, activityType, message, apiEndPoint, httpMethod);
 
 		} catch (Exception e) {
-			String message = "OTP code validation failed.";
+			message = "OTP code validation failed.";
+
 			logger.error("validateOtp Error: " + message);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(APIResponse.error(message));
+			HttpStatusCode htpStatuscode = e instanceof UserNotFoundException ? HttpStatus.NOT_FOUND
+					: HttpStatus.INTERNAL_SERVER_ERROR;
+			return handleResponseAndsendAuditLogForExceptionCase(e, htpStatuscode, activityType, activityDesc,
+					apiEndPoint, httpMethod);
+
 		}
+	}
+
+	private ResponseEntity<APIResponse<UserDTO>> handleResponseAndsendAuditLogForValidationFailure(
+			ValidationResult validationResult, String activityType, String activityDesc, String apiEndPoint,
+			String httpMethod) {
+		String message = validationResult.getMessage();
+		logger.error(message);
+		activityDesc = activityDesc.concat(message);
+		auditLogService.sendAuditLogToSqs(Integer.toString(validationResult.getStatus().value()),
+				validationResult.getUserId(), validationResult.getUserName(), activityType, activityDesc, apiEndPoint,
+				auditLogResponseFailure, httpMethod, message);
+		return ResponseEntity.status(validationResult.getStatus())
+				.body(APIResponse.error(validationResult.getMessage()));
+
+	}
+
+	private ResponseEntity<APIResponse<UserDTO>> handleResponseAndsendAuditLogForSuccessCase(UserDTO userDTO,
+			String activityType, String message, String apiEndPoint, String httpMethod) {
+		logger.info(message);
+		HttpStatus httpStatus = HttpStatus.OK;
+		auditLogService.sendAuditLogToSqs(Integer.toString(httpStatus.value()), userDTO.getUserID(),
+				userDTO.getUsername(), activityType, message, apiEndPoint, auditLogResponseSuccess, httpMethod, "");
+		return ResponseEntity.status(httpStatus).body(APIResponse.success(userDTO, message));
+	}
+
+	private <T> ResponseEntity<APIResponse<T>> handleResponseAndsendAuditLogForExceptionCase(Exception e,
+			HttpStatusCode htpStatuscode, String activityType, String activityDesc, String apiEndPoint,
+			String httpMethod) {
+		String message = e.getMessage();
+		String responseMessage = e instanceof UserNotFoundException ? e.getMessage() : genericErrorMessage;
+		logger.error("Error: " + message);
+		activityDesc = activityDesc.concat(message);
+		auditLogService.sendAuditLogToSqs(Integer.toString(htpStatuscode.value()), auditLogUserId, auditLogUserName,
+				activityType, activityDesc, apiEndPoint, auditLogResponseFailure, httpMethod, message);
+		return ResponseEntity.status(htpStatuscode).body(APIResponse.error(responseMessage));
 	}
 
 }
--- src/test/java/voucher/management/app/auth/controller/OTPControllerTest.java
+++ src/test/java/voucher/management/app/auth/controller/OTPControllerTest.java
@@ -1,13 +1,13 @@
 package voucher.management.app.auth.controller;
-
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test; 
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Mock; 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -18,11 +18,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import voucher.management.app.auth.dto.UserRequest;
-import voucher.management.app.auth.dto.ValidationResult;
+import voucher.management.app.auth.dto.*;
+import voucher.management.app.auth.enums.RoleType;
+import voucher.management.app.auth.exception.UserNotFoundException;
+import voucher.management.app.auth.service.impl.AuditLogService;
 import voucher.management.app.auth.service.impl.OTPStorageService;
+import voucher.management.app.auth.service.impl.UserService;
 import voucher.management.app.auth.strategy.impl.UserValidationStrategy;
 
 @SpringBootTest
@@ -33,67 +34,121 @@ import voucher.management.app.auth.strategy.impl.UserValidationStrategy;
 class OTPControllerTest {
 
     private MockMvc mockMvc;
-
+    
     @Mock
     private OTPStorageService otpService;
-
+    
     @Mock
     private UserValidationStrategy userValidationStrategy;
-
+    
+    @Mock
+    private UserService userService;
+    
+    @Mock
+    private AuditLogService auditLogService;
+    
     @InjectMocks
     private OTPController otpController;
-
+    
     private ObjectMapper objectMapper;
-
+    
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(otpController).build();
         objectMapper = new ObjectMapper();
     }
-
+    
     @Test
-    void testGenerateOtp() throws Exception {
-        UserRequest userRequest = new UserRequest();
-        userRequest.setEmail("test@example.com");
-        ValidationResult valid = new ValidationResult();
-        valid.setMessage("Valid");
-        valid.setStatus(HttpStatus.OK);
-        valid.setValid(true);
-         
-        when(userValidationStrategy.validateObject(userRequest.getEmail()))
-            .thenReturn(valid);
-
+    void testGenerateOtp_Success() throws Exception {
+        UserRequest userRequest = new UserRequest("test@example.com", null);
+        ValidationResult validationResult = new ValidationResult();
+        validationResult.setMessage("");
+        validationResult.setStatus(HttpStatus.OK);
+        validationResult.setValid(true);
+        
+        UserDTO userDTO = new UserDTO();
+        userDTO.setActive(true);
+        userDTO.setEmail("test@example.com");
+        userDTO.setRole(RoleType.MERCHANT);
+        userDTO.setVerified(true);
+        userDTO.setUserID("123412");
+        
+        when(userValidationStrategy.validateObject(userRequest.getEmail())).thenReturn(validationResult);
         when(otpService.generateAndStoreOTP(userRequest.getEmail())).thenReturn(123456);
-
+        when(userService.checkSpecificActiveUser("123")).thenReturn(userDTO);
+        
         mockMvc.perform(post("/api/otp/generate")
+                .header("X-User-Id", "123")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(userRequest)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.success").value(true));
+                .andExpect(jsonPath("$.message").value("OTP sent to test@example.com. It is valid for 10 minutes."));
     }
     
-
     @Test
-    void testOTPValidation() throws Exception {
-        UserRequest userRequest = new UserRequest();
-        userRequest.setEmail("test@example.com");
-        userRequest.setOtp(123456);
-        ValidationResult valid = new ValidationResult();
-        valid.setMessage("Valid");
-        valid.setStatus(HttpStatus.OK);
-        valid.setValid(true);
-         
-        when(userValidationStrategy.validateObject(userRequest.getEmail()))
-            .thenReturn(valid);
+    void testGenerateOtp_UserNotFound() throws Exception {
+        UserRequest userRequest = new UserRequest("test@example.com", null);
 
-        when(otpService.validateOTP(userRequest.getEmail(),userRequest.getOtp())).thenReturn(true);
-
+        ValidationResult validationResult = new ValidationResult();
+        validationResult.setMessage("");
+        validationResult.setStatus(HttpStatus.NOT_FOUND);
+        validationResult.setValid(false);
+        
+        when(userValidationStrategy.validateObject(userRequest.getEmail())).thenReturn(validationResult);
+        when(userService.checkSpecificActiveUser("123")).thenThrow(new UserNotFoundException("User not found"));
+        
+        mockMvc.perform(post("/api/otp/generate")
+                .header("X-User-Id", "123")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userRequest)))
+                .andExpect(status().isNotFound());
+    }
+    
+    @Test
+    void testValidateOtp_Success() throws Exception {
+        UserRequest userRequest = new UserRequest("test@example.com", "123456");
+        
+        ValidationResult validationResult = new ValidationResult();
+        validationResult.setMessage("");
+        validationResult.setStatus(HttpStatus.OK);
+        validationResult.setValid(true);
+        
+        UserDTO userDTO = new UserDTO();
+        userDTO.setActive(true);
+        userDTO.setEmail("test@example.com");
+        userDTO.setRole(RoleType.MERCHANT);
+        userDTO.setVerified(true);
+        userDTO.setUserID("123412");
+        
+        when(userValidationStrategy.validateObject(userRequest.getEmail())).thenReturn(validationResult);
+        when(otpService.validateOTP(userRequest.getEmail(), userRequest.getOtp())).thenReturn(true);
+        when(userService.checkSpecificActiveUser("123")).thenReturn(userDTO);
+        
         mockMvc.perform(post("/api/otp/validate")
+                .header("X-User-Id", "123")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(userRequest)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.success").value(true));
+                .andExpect(jsonPath("$.message").value("OTP is valid."));
     }
-
-
+    
+    @Test
+    void testValidateOtp_InvalidOtp() throws Exception {
+       UserRequest userRequest = new UserRequest("test@example.com", "123456");
+        
+        ValidationResult validationResult = new ValidationResult();
+        validationResult.setMessage("");
+        validationResult.setStatus(HttpStatus.BAD_REQUEST);
+        validationResult.setValid(false);
+         
+        when(userValidationStrategy.validateObject(userRequest.getEmail())).thenReturn(validationResult);
+        when(otpService.validateOTP(userRequest.getEmail(), userRequest.getOtp())).thenReturn(false);
+        
+        mockMvc.perform(post("/api/otp/validate")
+                .header("X-User-Id", "123")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userRequest)))
+                .andExpect(status().isBadRequest());
+    }
 }
+
