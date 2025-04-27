6940254dce9ece494cad81fb734e6f61a7575497
KhinYadanarNg <e0015793@u.nus.edu>
2025-04-28 00:26:29 +0800

    define minimum page and size value

--- src/main/java/voucher/management/app/auth/dto/SearchRequest.java
+++ src/main/java/voucher/management/app/auth/dto/SearchRequest.java
@@ -1,5 +1,6 @@
 package voucher.management.app.auth.dto;
 
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +8,10 @@ import lombok.Setter;
 @Setter
 public class SearchRequest {
 	    
+	    @Min(0)
 	    private int page = 0;
 
+	    @Min(1)
 	    private int size = 50;
 
 }
