ad431b32ff2f2781167adc280e4976e7da7fac30
KhinYadanarNg <e0015793@u.nus.edu>
2025-04-28 00:00:43 +0800

    added SearchRequest dto

---
+++ src/main/java/voucher/management/app/auth/dto/SearchRequest.java
@@ -0,0 +1,14 @@
+package voucher.management.app.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SearchRequest {
+	    
+	    private int page = 0;
+
+	    private int size = 50;
+
+}
